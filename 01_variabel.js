9138a52678164c85af497c484d4d9d04c5e70b28
sankhalanavin <navinmali2001@gmail.com>
2023-11-21 15:58:39 +0530

    variabel

---
+++ 01_variabel.js
@@ -0,0 +1,71 @@
+// console.log("hello world");
+//   let fullName = "sankhala navin";
+// console.log(fullName);
+//  let age = 24;
+//  let price = 99.99;
+//  x = null;
+//  console.log(x);
+
+//const age = 34;
+//age = 55;
+//console.log(age);
+
+ //++++++data Type++++++++++
+
+//+++++++++++++++primitive+++++++
+
+
+ //1. Number 
+//  let age= 24;
+//  console.log(age);
+//  console.log(typeof age );
+//  //2. String
+//  let name = "Sankhala Navin"
+//  console.log(name);
+//  console.log(typeof name);
+//  //3. Boolean
+//  let isFollwe =true;
+//  console.log( typeof isFollwe);
+//  //4. Undefined
+//  let x;
+//  console.log(typeof x);
+//  //5.Null
+//  let y = null;
+//  console.log(typeof y);
+//  //6.Bigint
+//  let z = BigInt("123")
+//  console.log(typeof z);
+//  //7.Symbol
+//  let a = Symbol("Hello");
+//  console.log( typeof a);
+
+//++++++++++++ non Primitive +++++
+
+
+//1. objects
+
+// 
+
+
+//exampel
+
+const product ={
+    name : " ball pen",
+    rating : 4,
+    offer : 5,
+    price :270,
+};
+// console.log(product);
+
+// console.log( typeof product);
+
+//2.ex
+const profile = {
+    username :"@navin",
+    isFollow : false,
+    followers: 342,
+    following :3243,
+};
+
+console.log(profile);
+console.log(typeof profile);
