ab155e5af3f3a650709b5665f6812b7f0e27962d
sankhalanavin <navinmali2001@gmail.com>
2023-11-23 12:52:02 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,75 @@
+// Loopss
+//1. For Loops
+//print 1 o 5
+
+// for (let i = 1; i <=5; i++){
+//         console.log("Sankhala Navin");
+// }
+   
+// 1 to n calulete sum 
+// let n=100;
+// let sum =0;
+// for(let i=1;i<=n;i++){
+//     sum = sum +i
+// }
+//  console.log("Sum is :", sum);
+
+
+// console.log("loop has ended");
+
+// 2. While loop
+    // let i=1;
+    // while(i<= 5){
+    // console.log("i is :",i);
+    // i++
+    // }
+
+//3. do while loop
+        // let i=1;
+        // do{
+        //     console.log("i is :",i);
+        //     i++
+        // }while (i<=5);
+
+//4. for-of loop 
+
+        // let str= "SankhalaNavin";
+        // let size = 0;
+        // for(let i of str){
+        //      console.log("i=",i);
+        //          size++;
+        // }
+        // console.log("String size=", size);
+            
+    //5, for - in loop
+    
+            // 
+ //practice 01           
+// let n=100;
+
+// for(let i=0;i<=n;i++){
+//     if(i%2!==0){
+//         console.log("num= ",i);
+//     }
+    
+// }
+
+// practice 02
+            let gameName=25;
+            let userNum= prompt("Guess the game Number:");
+
+            while(userNum != gameName){
+                userNum= prompt("You entered wrong number.Gess again:");
+            }
+            console.log("congratulations, you entered the right number");
+
+
+
+
+
+
+
+
+
+
+
