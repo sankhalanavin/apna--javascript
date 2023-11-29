7a4589649970a82f2bd3175d5b32b94fcdee144d
sankhalanavin <navinmali2001@gmail.com>
2023-11-29 11:28:20 +0530

    dom

--- javaS.js
+++ javaS.js
@@ -18,15 +18,51 @@
 
 
 //2.
-  let divs = document.querySelectorAll(".box");
-        let idx =1;
-        for(div of divs){
-            div.innerText = `new unique value ${idx}`;
-            idx++;
-        }
-
-
+  // let divs = document.querySelectorAll(".box");
+  //       let idx =1;
+  //       for(div of divs){
+  //           div.innerText = `new unique value ${idx}`;
+  //           idx++;
+  //       }
   //   console.dir(divs[0]);
 //   console.dir(divs[1]);
 //   console.dir(divs[2]);
+
+//Attributes
+
+//getattribute  (class name jova mate )
+//         let div = document.querySelector("div")
+//         console.log(div);
+
+//       let val = div.getAttribute("class");
+//          console.log(val);
+
+//          let name= div.getAttribute("name");
+//          console.log(name);
+
+// //setAttributes  (class name chanje karva mate)
+//          let para= div.setAttribute("class", "newClass");
+//          console.log(para);
+
+
+//Style is jS
+
+        // let div = document.querySelector("div");
+        // console.log(div);
+
+        // div.style.backgroundColor="green"
+
+//practice  Insert Element 
+
+let newBtn = document.createElement("button");
+newBtn.innerText="Click me!";
+
+
+newBtn.style.color="white";
+newBtn.style.backgroundColor="red";
+ document.querySelector("body").prepend(newBtn);
+
+
+
+
   
